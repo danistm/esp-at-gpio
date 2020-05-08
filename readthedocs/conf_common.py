420a3099dfa5a18cdf3bc39cc8d65872f96e6037
Xu Chun Guang <xuchunguang@espressif.com>
2020-05-08 20:30:49 +0800

    debug

--- readthedocs/conf_common.py
+++ readthedocs/conf_common.py
@@ -65,7 +65,10 @@ temp_sdkconfig_path = '{}/sdkconfig.tmp'.format(builddir)
 cur_dir = subprocess.check_output(["pwd"]).decode()
 print("cur_dir:%s"%(cur_dir))
 
-subprocess.check_call("make defconfig", shell=True, cwd="../..")
+result = subprocess.check_call("make defconfig", shell=True, cwd="../..")
+print("%s"%(result))
+result = subprocess.check_call("python -m pip install --user -r docs/requirements.txt", shell=True, cwd="../../esp-idf")
+print("%s"%(result))
 # note: trimming "examples" dir from KConfig/KConfig.projbuild as MQTT submodule
 # has its own examples in the submodule.
 kconfigs = subprocess.check_output(["find", "../../components",
